bd5683d79b6528bd5a230ee966ccc4cc72f64852
vovannmsk <vovuxa2013@yandex,ru>
2016-10-16 21:59:45 +0300

    dddddddddddddd

---
+++ Rasstanovka.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Вован on 09.05.2016.
+ */
+public class Rasstanovka {
+
+
+    public static void main(String[] args) {
+        System.out.println("Поцелуй мой блестящий металлический зад!");
+    }
+}
